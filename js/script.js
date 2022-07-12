64b9d9fa01743a019f7a934d2f89371697594a12
Nicolas <nicolas.ochkovsky@gmail.com>
2022-07-13 00:23:47 +0300

    Revision | Full adaptation header

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const burger = document.querySelector('.burger');
-const menuList = document.querySelector('.menu_list');
+const menu = document.querySelector('.menu');
+const headerBtn = document.querySelector('.header_btn');
 
 burger.addEventListener('click', ()=> {
     burger.classList.toggle('active');
-    menuList.classList.toggle('active');
+    menu.classList.toggle('active');
 });
 
 document.querySelectorAll('.menu_item-link').forEach(item => 
-item.addEventListener('click',() => {
-    burger.classList.toggle('active');
-    menuList.classList.toggle('active');
-}))
+    item.addEventListener('click',() => {
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+    }
+));
+headerBtn.querySelectorAll("[class]").forEach(item => 
+    item.addEventListener('click',()=>{
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+    }
+));
