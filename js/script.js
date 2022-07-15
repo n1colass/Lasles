62bd3fae6b9134c2c015faa248db44df9a2902ba
Nicolas <nicolas.ochkovsky@gmail.com>
2022-07-15 23:36:48 +0300

    Section Price - Fully adaptation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
-const headerBtn = document.querySelector('.header_btn');
+const btn = document.querySelector('.btn');
 
 burger.addEventListener('click', ()=> {
     burger.classList.toggle('active');
@@ -13,7 +13,7 @@ document.querySelectorAll('.menu_item-link').forEach(item =>
         menu.classList.toggle('active');
     }
 ));
-headerBtn.querySelectorAll("[class]").forEach(item => 
+btn.querySelectorAll("[class]").forEach(item => 
     item.addEventListener('click',()=>{
         burger.classList.toggle('active');
         menu.classList.toggle('active');
