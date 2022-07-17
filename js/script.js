f5ca40b80cb8d94923a580353ab246694e2efb12
Nicolas <nicolas.ochkovsky@gmail.com>
2022-07-18 02:08:40 +0300

    Section Testimonials ready | Fully Adaptation

--- js/script.js
+++ js/script.js
@@ -24,8 +24,23 @@ const swiper = new Swiper('.swiper',{
     direction: 'horizontal',
     slidesPerView: 3,
     spaceBetween: 50,
+    navigation: {
+        nextEl: '.swiper-leaf-next',
+        prevEl: '.swiper-leaf-prev',
+      },
     pagination: {
         el: ".swiper-pagination-main",
         clickable: true,
+    },
+    breakpoints: {
+        1200:{
+            slidesPerView: 3,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        100:{
+            slidesPerView: 1,
+        }
     }
 });
